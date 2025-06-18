974204d19f10dc3bdd2854b39a12bcc23f00392c
canvalogo <bahtijoratadzanov5@gmail.com>
2025-06-18 18:36:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let name = prompt('ismizi soravoman')
+let age = +prompt('yoshiziyam yozvorin oka')
+
+console.log(name + age);
+console.log(typeof(name)  +  typeof(age) );
+console.log('type name '+  Boolean(name) );
